198ecd7e44c100e9d827ee494d278ab37b697c59
范綱栓 <harry750110@gmail.com>
2025-05-20 15:12:26 +0800

    更新路由

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	base: '/harry-blog/', // 子路徑部署
 });
